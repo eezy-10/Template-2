48df67e7b46ec044b60fef03da2021988f75e793
Irtaza Ahmad <ahmadirtaza10@gmail.com>
2025-06-03 20:20:44 +0500

    Added Services and removed slideshow function.

--- script.js
+++ script.js
@@ -24,18 +24,18 @@ let showingFirst = true;
 let sltxt1 = slText1.textContent;
 let sltxt2 = slText2.textContent;
 
-setInterval(() => {
-  if (showingFirst) {
-    slImg.src = './slide2.jpg';
-    slText1.textContent = 'NEW GENERATION OF SKATING';
-    slText2.textContent = 'lorem dipsum ioler sit amit, consectetur adipisicing elit. Est odit dolorum voluptates.';
-  } else {
-    slImg.src = './slide1.jpg';
-    slText1.textContent = sltxt1;
-    slText2.textContent = sltxt2;
-  }
-  showingFirst = !showingFirst;
-}, 5000);
+// setInterval(() => {
+//   if (showingFirst) {
+//     slImg.src = './slide2.jpg';
+//     slText1.textContent = 'NEW GENERATION OF SKATING';
+//     slText2.textContent = 'lorem dipsum ioler sit amit, consectetur adipisicing elit. Est odit dolorum voluptates.';
+//   } else {
+//     slImg.src = './slide1.jpg';
+//     slText1.textContent = sltxt1;
+//     slText2.textContent = sltxt2;
+//   }
+//   showingFirst = !showingFirst;
+// }, 5000);
 
 
 const slBtn = document.querySelectorAll('.slBtn');
